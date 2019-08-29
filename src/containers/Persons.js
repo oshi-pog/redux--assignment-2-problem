4bdad8e52de161e7913258956122160710e072e7
Joshua Kaplan <joshkap2015@gmail.com>
2019-08-29 12:25:19 +0200

    wip

---
+++ src/containers/Persons.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+import * as actionTypes from '../store/actions/action';
+
+class Persons extends Component {
+  state = {
+    persons: []
+  };
+
+  personAddedHandler = () => {
+    const newPerson = {
+      id: Math.random(), // not really unique but good enough here!
+      name: 'Max',
+      age: Math.floor(Math.random() * 40)
+    };
+    this.setState(prevState => {
+      return { persons: prevState.persons.concat(newPerson) };
+    });
+  };
+
+  personDeletedHandler = personId => {
+    this.setState(prevState => {
+      return {
+        persons: prevState.persons.filter(person => person.id !== personId)
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <AddPerson personAdded={this.personAddedHandler} />
+        {this.state.persons.map(person =>
+          <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            clicked={() => this.personDeletedHandler(person.id)}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    persons: state.persons
+  };
+};
+
+export default connect(mapStateToProps)(Persons);
---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,14 @@
+import * as actionTypes from '../../store/actions/action';
+
+const initialState = {
+  counter: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  }
+
+  return state;
+};
+
+export default reducer;
